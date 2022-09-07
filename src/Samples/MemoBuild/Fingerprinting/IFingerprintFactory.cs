ad3ad02241835f9a627ccf4b0ee1fdc8eac9177f
David Federman <dfederm@microsoft.com>
2022-09-07 13:35:08 -0700

    MemoBuild plugin: Implement fingerprinting

---
+++ src/Samples/MemoBuild/Fingerprinting/IFingerprintFactory.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+
+namespace MemoBuild.Fingerprinting
+{
+    internal interface IFingerprintFactory
+    {
+        byte[]? GetWeakFingerprint(NodeContext nodeContext);
+
+        PathSet? GetPathSet(NodeContext nodeContext, IEnumerable<string> observedInputs);
+
+        byte[]? GetStrongFingerprint(PathSet? pathSet);
+    }
+}
