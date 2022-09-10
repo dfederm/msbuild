12cbf4468839455145e240a2bedab20c2d9ed712
David Federman <dfederm@microsoft.com>
2022-09-09 17:08:52 -0700

    MemoBuild plugin: Implement output hasher

---
+++ src/Samples/MemoBuild/Hashing/IOutputHasher.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using BuildXL.Cache.ContentStore.Hashing;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MemoBuild.Hashing
+{
+    internal interface IOutputHasher : IAsyncDisposable
+    {
+        Task<ContentHash> ComputeHashAsync(string filePath, CancellationToken cancellationToken);
+    }
+}
